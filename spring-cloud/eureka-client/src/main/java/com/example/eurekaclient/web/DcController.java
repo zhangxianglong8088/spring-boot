92d6b98c62f502c6a48ec4c28afe6553e4e31f08
zhangxianglong006 <zhangxianglong006@ke.com>
2022-02-21 11:35:40 +0800

    eureka server

---
+++ spring-cloud/eureka-client/src/main/java/com/example/eurekaclient/web/DcController.java
@@ -0,0 +1,26 @@
+package com.example.eurekaclient.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @description：
+ * @author: zhangxianglong
+ * @date: 2022/2/21
+ */
+@RestController
+public class DcController {
+    @Resource
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
