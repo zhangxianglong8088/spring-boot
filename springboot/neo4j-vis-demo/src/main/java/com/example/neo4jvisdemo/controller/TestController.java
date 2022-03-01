be08a99e1e9aa59e5942b2bc7db9311f1e42d490
zhangxianglong006 <zhangxianglong006@ke.com>
2022-03-01 15:33:48 +0800

    整合页面

--- springboot/neo4j-vis-demo/src/main/java/com/example/neo4jvisdemo/controller/TestController.java
+++ springboot/neo4j-vis-demo/src/main/java/com/example/neo4jvisdemo/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,7 +13,7 @@ import java.util.Set;
 
 @RestController
 public class TestController {
-    @Autowired
+    @Resource
     private Neo4jUtil neo4jUtil;
 
     @GetMapping("get")
